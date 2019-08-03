86fec980e1ec5d2552d0a30d1ea495bb05155755
Ross Murray <rossmichaelm@gmail.com>
2019-08-03 15:03:03 +0100

    before css delete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-// /client/App.js
-import React, {Component} from 'react';
-// import TutorialExample from './TutorialExample'
+import React, { Component } from 'react';
 import NHSHeader from './page/NHSHeader';
-import './css/App.css';
 import NHSFooter from './page/NHSFooter';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Start from './Start';
 import NewQuestion from './components/NewQuestion';
 import ShowQuestions from './components/ShowQuestions'
 import Login from './components/Login'
+import { Container } from '@material-ui/core';
 
 
 // process.on('unhandledRejection', (reason, p) => {
@@ -20,46 +18,25 @@ class App extends Component {
 
     myNewQuestion = (props) => {
         const myProps = {
-            question: {questionText: "hello there"}
+            question: { questionText: "hello there" }
         }
         return (
-            <NewQuestion {...myProps}/>
+            <NewQuestion {...myProps} />
         )
     };
 
     render() {
         return (
             <Router>
-                <div className="mh_container">
-                    <div className="mh_header">
-
-                        <NHSHeader/>
-                    </div>
-                    {/*<TutorialExample/>*/}
-                    <div className="mh_body">
-                        <div className="nhsuk-width-container">
-                            <Route exact path="/" component={Start}/>
-
-                            {/*<Link to="/what"><h1>Some other link</h1></Link>*/}
-                            {/*<Route exact path="/about" component={MHQuestions} />*/}
-                            <Route exact path="/admin/newQuestion" component={NewQuestion} />
-                            <Route exact path="/login" component={Login} />
-                            <Route exact path="/editQuestions" component={ShowQuestions} />
-
-                            {/* render={(routeProps) => (
-    <MyComponent {...routeProps} {...props} />
-  )} */}
-                            {/* <Route exact path="/register" component={Register} /> */}
-                            {/* <ShowQuestions/> */}
-                        </div>
-                    </div>
-                    <div className="mh_footer">
-                        <NHSFooter/>
-                    </div>
-                    {/*<Route exact path="/what" component={MHQuestions} />*/}
-                </div>
-            </Router>
-
+                <NHSHeader />
+                <Container >
+                    <Route exact path="/" component={Start} />
+                    <Route exact path="/admin/newQuestion" component={NewQuestion} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/editQuestions" component={ShowQuestions} />
+                </Container>
+                <NHSFooter />
+            </Router >
         )
     }
 }
