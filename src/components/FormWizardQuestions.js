cc91dc4099cdf83d3c80774a8e4088c6396e4407
Ross Murray <rossmichaelm@gmail.com>
2019-08-22 18:03:51 +0100

    created form wizard

--- src/components/FormWizardQuestions.js
+++ src/components/FormWizardQuestions.js
@@ -10,30 +10,6 @@ export const FormWizardQuestions = props => {
     const form = props.form
     const firstQuestion = props.firstQuestion
 
-    // const [form, setForm] = useState({ title: '' })
-    // const [currentQuestion, setCurrentQuestion] = useState({questionText: ''})
-
-    // useEffect(() => {
-    //     const formPromise = getFormsFromAPI().then(forms => {
-    //          // get only the relevant form
-    //         const formID = match.params.formid
-    //         const currentForm = forms.find(form => form._id === formID)
-    //         setForm(currentForm)
-    //         return currentForm
-    //     })
-    //     const questionsPromise = getQuestions()
-
-    //     // only run after received all forms and questions
-    //     Promise.all([formPromise, questionsPromise]).then(([form, questions]) => {
-    //         console.log(form)
-    //         const firstQuestion = questions.find(question => form.firstQuestion === question._id)
-    //         setCurrentQuestion(firstQuestion)
-    //         console.log(questions)
-    //         // setForm(formPromise)
-    //     })
-
-    // }, [match.params.formid])
-
 
     return (
         <div>
