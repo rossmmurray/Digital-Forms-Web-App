0a32fc55a68e4d1f71fa70cf9d064785ec7ebb7d
Ross Murray <rossmichaelm@gmail.com>
2019-09-04 21:02:54 +0100

    git rid of drinkline (all related files now)

--- src/components/temp.js
+++
@@ -1,4 +0,0 @@
-<h3>Drinkline</h3>
-<p>Drinkline runs a free, confidential helpline for people who are concerned about their drinking, or someone else's.</p>
-<p>The purpose of the Drinkline service is to offer free, confidential, accurate and consistent information and advice to callers who are concerned about their own or someone else`s drinking regardless of the caller`s age, gender, sexuality, ethnicity or spirituality.</p>
-<h4><strong>Free helpline: 0300 123 1110 (weekdays 9am&ndash8pm, weekends 11am&ndash4pm)</strong></h4>
