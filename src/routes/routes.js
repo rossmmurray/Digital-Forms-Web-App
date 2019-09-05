6fca9de53a09b40f81636a1c65ac0084dc274345
Ross Murray <rossmichaelm@gmail.com>
2019-09-05 05:18:06 +0100

    added one root

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,12 @@
 // import { Home, AccountCircle, PeopleOutline, ListAlt, QuestionAnswer, Edit, AddComment } from '@material-ui/icons';
+import { Home } from '@material-ui/icons'
 
 
-export const routes = []
+export const routes = [
+  {
+    path: "/",
+    name: 'Home',
+    icon: Home,
+  }
+  }
+]
