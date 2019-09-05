cbefef911d7f5cb06bf355b5a9356e76728ae20d
Ross Murray <rossmichaelm@gmail.com>
2019-09-05 15:17:47 +0100

    removed the /Home part

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,6 @@
 // import { Home, AccountCircle, PeopleOutline, ListAlt, QuestionAnswer, Edit, AddComment } from '@material-ui/icons';
 // import React from 'react';
-import { Home } from '@material-ui/icons/Home'
+import { Home } from '@material-ui/icons'
 
 
 export const routes = [
