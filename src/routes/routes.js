5e87950f7b20dc1f0a34a94ba75628e5ee365ff0
Ross Murray <rossmichaelm@gmail.com>
2019-09-05 13:38:48 +0100

    removed home import

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 // import { Home, AccountCircle, PeopleOutline, ListAlt, QuestionAnswer, Edit, AddComment } from '@material-ui/icons';
-import { Home } from '@material-ui/icons'
+// import { Home } from '@material-ui/icons'
 
 
 export const routes = [
