bf26e146ffe1c9913a265aab50afc98cda2833b0
Ross Murray <rossmichaelm@gmail.com>
2019-09-05 14:09:55 +0100

    added home import back and added React

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 // import { Home, AccountCircle, PeopleOutline, ListAlt, QuestionAnswer, Edit, AddComment } from '@material-ui/icons';
-// import { Home } from '@material-ui/icons'
+import React from 'react';
+import { Home } from '@material-ui/icons/Home'
 
 
 export const routes = [
