6a48ca54aecc8ebbc3d4149f51f9a3bd519c1423
Ross Murray <rossmichaelm@gmail.com>
2019-09-05 14:59:21 +0100

    no React import

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 // import { Home, AccountCircle, PeopleOutline, ListAlt, QuestionAnswer, Edit, AddComment } from '@material-ui/icons';
-import React from 'react';
+// import React from 'react';
 import { Home } from '@material-ui/icons/Home'
 
 
