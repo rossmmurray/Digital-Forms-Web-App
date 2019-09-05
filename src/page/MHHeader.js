ec6be68af057f175066b57b69fbfba06140b0573
Ross Murray <rossmichaelm@gmail.com>
2019-09-05 02:34:31 +0100

    nearly added drawer back

--- src/page/MHHeader.js
+++ src/page/MHHeader.js
@@ -10,18 +10,70 @@ import { Link } from "react-router-dom";
 import { getFormsFromAPI } from '../helper/ApiDataFunctions';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MailIcon from '@material-ui/icons/Mail';
+import routes from '../routes/routes';
+import { Toc } from '@material-ui/icons';
+import { ListSubheader } from '@material-ui/core';
+
+
+const drawerWidth = 240
 
 const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
     },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
     title: {
         flexGrow: 1,
-        fontWeight: "bold",
+        fontWeight: 900,
         fontStyle: "italic"
+    },
+    subtitle: {
+        flexGrow: 1,
+        fontWeight: 200,
+        // fontStyle: "italic"
+    },
+    drawer: {
+        [theme.breakpoints.up('sm')]: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+    },
+    appBar: {
+        marginLeft: drawerWidth,
+        [theme.breakpoints.up('sm')]: {
+            width: `calc(100% - ${drawerWidth}px)`,
+        },
+    },
+    //   don't display menu button if small
+    menuButton: {
+        marginRight: theme.spacing(2),
+        [theme.breakpoints.up('sm')]: {
+            display: 'none',
+        },
+    },
+    toolbar: {
+        display: 'flex',
+        backgroundColor: "primary",
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: theme.spacing(0, 1),
+        ...theme.mixins.toolbar,
+
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
     },
 }));
 
@@ -31,6 +83,12 @@ export default function ButtonAppBar() {
 
     const [forms, setForms] = useState([])
     const [menuAnchor, setMenuAnchor] = useState(null)
+        // const theme = useTheme();
+        const [mobileOpen, setMobileOpen] = React.useState(false);
+
+        function handleDrawerToggle() {
+            setMobileOpen(!mobileOpen);
+        }
 
     useEffect(() => {
         getFormsFromAPI().then(forms => {
@@ -43,7 +101,7 @@ export default function ButtonAppBar() {
         setMenuAnchor(event.currentTarget)
     }
 
-    const handleClose = event => {
+    const handleClose = () => {
         setMenuAnchor(null)
     }
 
