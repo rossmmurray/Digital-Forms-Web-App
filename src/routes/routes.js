63fccb53eeec9b8baa3c6155baeb457beba69508
Ross Murray <rossmichaelm@gmail.com>
2019-09-05 04:53:39 +0100

    routes does nothing basically

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,40 +1,4 @@
-import { Home, AccountCircle, PeopleOutline, ListAlt, QuestionAnswer, Edit, AddComment } from '@material-ui/icons';
+// import { Home, AccountCircle, PeopleOutline, ListAlt, QuestionAnswer, Edit, AddComment } from '@material-ui/icons';
 
 
-export const routes = [
-  {
-    path: "/",
-    name: 'Home',
-    icon: Home,
-  },
-  {
-    path: '/Login',
-    name: 'Login',
-    icon: AccountCircle,
-  },
-  {
-    path: "/admin/newQuestion",
-    name: 'Add Question',
-    icon: AddComment,
-  },
-  {
-    path: '/editQuestions',
-    name: 'Edit Questions',
-    icon: Edit,
-  },
-  {
-    path: "/answers",
-    name: 'View Answers',
-    icon: QuestionAnswer,
-  },
-  {
-    path: "/admin/userManagement",
-    name: 'Manage Users',
-    icon: PeopleOutline,
-  },
-  {
-    path: "/admin/manageForms",
-    name: 'Manage Forms',
-    icon: ListAlt,
-  }
-]
+export const routes = []
